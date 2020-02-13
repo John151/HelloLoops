03acc022e4112bac076ccdf4b54dc2c9037f757d
John Cokins <il7148pg@go.minnstate.edu>
2020-02-13 00:55:58 -0600

    Add project files.

---
+++ HelloLoops/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HelloLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.numLoops = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numLoops)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(29, 62);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnWhile.TabIndex = 0;
+            this.btnWhile.Text = "While Loop";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Location = new System.Drawing.Point(144, 62);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(104, 23);
+            this.btnDoWhile.TabIndex = 1;
+            this.btnDoWhile.Text = "Do While Loop";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(292, 62);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.TabIndex = 2;
+            this.btnFor.Text = "For Loop";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // numLoops
+            // 
+            this.numLoops.Location = new System.Drawing.Point(292, 36);
+            this.numLoops.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numLoops.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numLoops.Name = "numLoops";
+            this.numLoops.Size = new System.Drawing.Size(120, 20);
+            this.numLoops.TabIndex = 3;
+            this.numLoops.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numLoops.ValueChanged += new System.EventHandler(this.numLoops_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How many would you like?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 151);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numLoops);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numLoops)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.NumericUpDown numLoops;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
